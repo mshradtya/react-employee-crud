dc7b9119ad0a85764245bbcb7491b3e5c1c8e0b3
mshradtya <adityapmishra196@gmail.com>
2023-01-07 20:10:47 +0530

    dashboard component basic setup

--- src/page/Dashboard/index.js
+++ src/page/Dashboard/index.js
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Swal from "sweetalert2";
+
+import Header from "./Header";
+import List from "./List";
+import Add from "./Add";
+import Edit from "./Edit";
 
 import { employeesData } from "../../data";
 
 function Dashboard() {
-  return <div></div>;
+  const [employees, setEmployees] = useState(employeesData);
+  const [selectedEmployee, setSelectedEmployee] = useState(null);
+  const [isAdding, setIsAdding] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEdit = () => {};
+  const handleDelete = () => {};
+
+  return (
+    <div className="container">
+      {/* List */}
+      {!isAdding && !isEditing && (
+        <>
+          <Header setIsAdding={setIsAdding} />
+          <List
+            employees={employees}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+          />
+        </>
+      )}
+      {/* Add */}
+      {!isAdding && (
+        <Add
+          employees={employees}
+          setEmployees={setEmployees}
+          setIsAdding={setIsAdding}
+        />
+      )}
+      {/* Edit */}
+      {!isEditing && (
+        <Edit
+          employees={employees}
+          selectedEmployee={selectedEmployee}
+          setEmployees={setEmployees}
+          setIsEditing={setIsEditing}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Dashboard;
